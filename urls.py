b55da52f272c1c5ac5e955d2d9f9542ed784b65c
JIIMONCHIK <artmvas38@Gmail.com>
2025-07-07 18:25:42 +0300

    first steps

---
+++ urls.py
@@ -0,0 +1,13 @@
+# from django.urls import path
+#
+# from . import views
+#
+# urlpatterns = [
+#     path("articles/2003/", views.special_case_2003),
+#     path("articles/<int:year>/", views.year_archive),
+#     path("articles/<int:year>/<int:month>/", views.month_archive),
+#     path("articles/<int:year>/<int:month>/<slug:slug>/", views.article_detail),
+# ]
+
+# A request to /articles/2005/03/ would match the third entry in the list.
+# Django would call the function views.month_archive(request, year=2005, month=3).
